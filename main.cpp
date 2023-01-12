3b6f189e57264a43f51ef716acb524c6301238d7
Sanjay R <sanjay66r@gmail.com>
2023-01-12 16:27:20 +0530

    added image

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <GLUT/glut.h>
-#include<OpenGL/gl.h>
-#include<OpenGL/glu.h>  
+#include <GL/glut.h>
+#include<GL/gl.h>
+#include<GL/glu.h>  
 #include<math.h>
 #include<stdlib.h>
 
